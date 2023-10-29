95544cb04996ef72d76d6aa4a14a7f9c0090d49b
szsmommy <szsmommy@gmail.com>
2023-10-28 20:35:43 -0700

    Corrected return on incorrect answers

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-$("#quiz-form").on("submit", function(event) {
-    event.preventDefault();
-    var $answer = $("#quiz-answer"); 
-    var answer = $answer.val();
-    console.log(answer);
-    if (answer = "Joe Biden"){
-        $("#result").text("Yay! You answered correctly!");
-    } else {
-        $("#result").text("Sorry, try again.")
-        $else("p").css("border", "5px solid red");
-    }
+$(document).ready(function() {
+    $("#quiz-form").submit(function(event) {
+        event.preventDefault();
+        var $answer = $("#quiz-answer"); 
+        var answer = $answer.val();
+        console.log(answer);
+        if (answer === "Joe Biden") {
+            $("#result").text("Yay! You answered correctly!").css("color", "green");
+        } else {
+            $("#result").text("Sorry, try again.").css("color", "red");
+        }
+        $answer.prop('disabled', true);
+        $('option').not(':selected').attr('disabled', true);
+    });
 });
